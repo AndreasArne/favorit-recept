39df6a5e324ab716ddb79efd6622922d86b7adbc
Andreas Arnesson <andreas_osby@hotmail.com>
2022-07-13 16:53:41 +0200

    fixed spelling

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -3,4 +3,4 @@
 </svelte:head>
 <h1>Kontakta mig</h1>
 
-<p>Om du har några förslag på ändringar eller förbättringar, skickar ett mejl till <a class="link" href="mailto:andreas_a+favoritrecept@outlook.com">andreas_a+favoritrecept@outlook.com</a>!</p>
+<p>Om du har några förslag på ändringar eller förbättringar, skicka ett mejl till <a class="link" href="mailto:andreas_a+favoritrecept@outlook.com">andreas_a+favoritrecept@outlook.com</a>!</p>
