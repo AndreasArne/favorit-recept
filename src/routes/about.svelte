916d900dd9e4b1ac1402be98f34d9b9941eeb2be
Andreas Arnesson <andreas_osby@hotmail.com>
2022-07-13 16:58:19 +0200

    added link to repo on about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,3 +4,5 @@
 <h1>Om Olsson-släktens favoritrecept</h1>
 
 <p>Detta är en digitalisering av Olsson-släktens favoritrecept som skrevs ner 1998. Webbsidan utvecklas av Andreas Arnesson, barnbarn till Siv och Arne Olsson.</p>
+
+<p>Koden för denna webbsida finns i tillgänglig på <a class="link" href="https://github.com/AndreasArne/favorit-recept">https://github.com/AndreasArne/favorit-recept</a>.</p>
