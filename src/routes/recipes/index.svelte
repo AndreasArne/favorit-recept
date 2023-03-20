cfbadce2b7f1e532891c7abedf9b503a5fda6efd
Andreas Arnesson <andreas_osby@hotmail.com>
2023-03-20 22:43:42 +0100

    improved search code. Uses store

--- src/routes/recipes/index.svelte
+++ src/routes/recipes/index.svelte
@@ -9,23 +9,29 @@ export const load = async ({ fetch }) => {
     }
   }
 }
+
 </script>
 
 <script>
+  export let recipes
   import { fade, scale, } from 'svelte/transition'
   import { flip } from 'svelte/animate'
+  import { onDestroy } from "svelte";
   import Search from '$lib/components/Search.svelte'
-  export let recipes
-  let searchValue = ""
-  let filteredRecipes = []
 
-    $: {
-        if(searchValue) {
-            filteredRecipes = recipes.filter( recipe => recipe.meta.title.toLowerCase().includes(searchValue.toLowerCase()));
-        } else {
-            filteredRecipes = [...recipes];
-        }
-    }
+  import { searchHandler, createSearchStore } from "../../lib/stores/search";
+
+  const searchRecipes = recipes.map((recipe) => ({
+    ...recipe,
+    searchTerms: `${recipe.meta.title}`,
+  }))
+
+  const searchStore = createSearchStore(searchRecipes)
+  const unsubscribe = searchStore.subscribe((model) => searchHandler(model))
+
+  onDestroy(() => {
+    unsubscribe();
+  })
 </script>
 
 
@@ -33,10 +39,10 @@ export const load = async ({ fetch }) => {
 <div class="px-16 py-6 md:col-span-2">
   <h1>Recept</h1>
 
-  <Search bind:searchValue={searchValue}/>
+  <Search bind:searchValue={$searchStore.search}/>
 
   <div class="mt-8 grid lg:grid-cols-3 gap-10">
-    {#each filteredRecipes as recipe, i (i)}
+    {#each $searchStore.filtered as recipe, i (i)}
     <a class="card hover:shadow-lg" href={recipe.path} in:fade out:scale|local animate:flip={{duration: 800}}>
         <div class="mx-auto text-center flex flex-col justify-center h-full">
           <span class="font-bold">{recipe.meta.title}</span>
